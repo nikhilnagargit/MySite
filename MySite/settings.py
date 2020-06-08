bb339c88953a0ddf6d5bae69987dc06676486b39
nikhilnagargit <nikhilnagarcps@gmail.com>
2020-06-08 10:16:13 +0530

    final commit

--- MySite/settings.py
+++ MySite/settings.py
@@ -133,5 +133,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
+# fdfsd
 LOGIN_REDIRECT_URL = '/'
