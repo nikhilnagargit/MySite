f994e76f1d0b97c725d16780ba2187c02ca94061
nikhilnagargit <nikhilnagarcps@gmail.com>
2020-06-08 09:47:33 +0530

    final

--- MySite/settings.py
+++ MySite/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
